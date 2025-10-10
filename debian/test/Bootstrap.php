36ca4ec3d36cbb5c38bc43a37cea0e88d3d465a9
CyberVitexus <info@vitexsoftware.cz>
2025-10-10 18:35:38 +0200

    Add Debian packaging structure

---
+++ debian/test/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Bootstrap for PHPUnit tests for php-vitexsoftware-ease-doctrineorm
+ *
+ * @author Vitex <info@vitexsoftware.cz>
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
